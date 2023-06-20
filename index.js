b635f88d14a0bd38f2998c89415b431172a4a92d
Parth Joshi <parth.00.joshi@gmail.com>
2023-06-20 23:48:23 +0530

    wiring the sockets between client & server

--- index.js
+++ index.js
@@ -15,4 +15,16 @@ var io = socket(server);
 
 io.on('connection', function (socket) {
 	console.log('socket connection established', socket.id);
+
+	socket.on('discuss', function (data) {
+		io.sockets.emit('discuss', data);
+	});
+
+	socket.on('typing', function (data) {
+		socket.broadcast.emit('typing', data);
+	});
+
+	socket.on('success', function () {
+		socket.broadcast.emit('success');
+	});
 });
--- public/samvad.js
+++ public/samvad.js
@@ -1,3 +1,39 @@
 // connect backend socket
-
 const socket = io.connect('http://localhost:7777/');
+
+// get DOM elements
+const output = document.getElementById('output');
+const feedback = document.getElementById('feedback');
+const senderUser = document.getElementById('senderUser');
+const message = document.getElementById('message');
+const sendBtn = document.getElementById('sendBtn');
+
+// add event listeners and emit events
+sendBtn.addEventListener('click', function () {
+	if (message.value) {
+		socket.emit('discuss', {
+			senderUser: senderUser.value,
+			message: message.value
+		});
+		message.value = '';
+		senderUser.disabled = true;
+	}
+	socket.emit('success');
+});
+
+message.addEventListener('keypress', function () {
+	socket.emit('typing', senderUser.value);
+});
+
+// listen for socket events
+socket.on('discuss', function (data) {
+	output.innerHTML += `<p>${data.senderUser}: ${data.message}</p>`;
+});
+
+socket.on('typing', function (sender) {
+	if (sender) feedback.innerHTML = `<p><em>${sender} is typing..</em></p>`;
+});
+
+socket.on('success', function () {
+	feedback.innerHTML = '';
+});
